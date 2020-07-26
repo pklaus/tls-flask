1c141bec69067c3eb7e500c7bf44b73b99e8e304
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-07-26 19:54:08 +0200

    consolidated everything in server.py

--- server.py
+++ server.py
@@ -6,14 +6,12 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 from pprint import pformat
 
-from request_properties import REQUEST_PROPERTIES
-
 app = Flask(__name__)
 
 # formatter = HtmlFormatter(style="colorful")
 formatter = HtmlFormatter()
 
-template = """
+TEMPLATE = """
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -27,6 +25,15 @@ template = """
 </html>
 """.strip()
 
+REQUEST_PROPERTIES = [
+    "environ", "path", "full_path", "script_root", "url", "base_url", "url_root", "accept_charsets", "accept_encodings",
+    "accept_languages", "accept_mimetypes", "access_control_request_headers", "access_control_request_method",
+    "access_route", "args", "authorization", "base_url", "blueprint", "cache_control", "content_encoding", "content_length",
+    "content_md5", "content_type", "cookies", "data", "date", "endpoint", "files", "form", "full_path", "headers", "host",
+    "host_url", "if_match", "if_modified_since", "if_none_match", "if_range", "if_unmodified_since", "is_json",
+    "is_multiprocess", "is_multithread", "is_run_once", "is_secure", "json", "max_content_length", "max_forwards", "method",
+    "mimetype", "mimetype_params", "origin",
+]
 
 @app.route("/")
 def index():
@@ -37,7 +44,7 @@ def index():
         data, indent=1, width=80, depth=None, compact=False
     )  # py38: sort_dicts=True
     content = highlight(data, get_lexer_by_name("python"), formatter)
-    return render_template_string(template, content=content)
+    return render_template_string(TEMPLATE, content=content)
 
 
 @app.route("/pygments.css")
